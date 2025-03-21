5053b5a0741747bb1523a6b13c97ce0727aa1a39
nhi-batau <ssmemes163@gmail.com>
2025-03-21 19:25:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
-    API_ID = 22581733
-    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
-    ADMIN = '6530997270'.split(',')
+    BOT_TOKEN = "7822801684:AAE4y0ETMqA12Y8xakRofTfWIsEG3IGXhYw"
+    API_ID = 20114039
+    API_HASH = "87297b8f3cc8fc9bbce591ad30da5896"
+    ADMIN = '8172163893'.split(',')
     ADMIN_ID = [int(id) for id in ADMIN]
-    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_URL = "mongodb+srv://mewadonlinestudy:yYjxKiZGQag8DnWH@cpvod.ywpecvo.mongodb.net/?retryWrites=true&w=majority&appName=cpvodak"
     DB_NAME = "Crack_Jack"
-    TXT_LOG = -1002294532244
-    AUTH_LOG = -1002294532244
-    HIT_LOG = -1002294532244
-    DRM_DUMP = -1002294532244
-    CHANNEL = --1002487777733
-    CH_URL = "https://t.me/justfortestinggwak"
-    OWNER = "https://t.me/mrcraacker"
+    TXT_LOG = -1002647652455
+    AUTH_LOG = -1002647652455
+    HIT_LOG = -1002647652455
+    DRM_DUMP = -1002647652455
+    CHANNEL = -1002274225498
+    CH_URL = "https://t.me/BHUMIHAR_BOTSS"
+    OWNER = "https://t.me/Thebhumihar"
     THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
     HOST = "https://drm-api-five.vercel.app"
